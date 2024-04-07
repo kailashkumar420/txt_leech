add2074379f726343d4fde5df6c80b2315c77764
kailashkumar420 <166263798+kailashkumar420@users.noreply.github.com>
2024-04-07 06:03:23 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "28337707"
+api_hash = "1148b0caf53d932a910c37eee71e3bcc"
+bot_token = "6943268950:AAFSv7tFCfeFljIZLzEqph9F5t7WWDm1QQE"
