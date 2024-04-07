3be447bbb4e91e47ec00198a8c09152989b3e7ad
kailashkumar420 <166263798+kailashkumar420@users.noreply.github.com>
2024-04-07 07:51:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,2 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "28337707"
+api_hash = "6943268950:AAFSv7tFCfeFljIZLzEqph9F5t7WWDm1QQE"
